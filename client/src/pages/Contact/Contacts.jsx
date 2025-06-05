0eb3251f26fe0824225733d931de63d5e414388d
Joel Mbithi <joellembithi@gmail.com>
2025-06-05 18:34:29 +0300

    Almost done with Contact page

--- client/src/pages/Contact/Contacts.jsx
+++ client/src/pages/Contact/Contacts.jsx
@@ -8,20 +8,22 @@ const Contacts = () => {
       <p className='heading-text'>Have questions or need assistance? We're here to help! <br /> Reach out to our team through any of the channels below.</p>
       </div>
 
-      {/* left side */}
+      <div className='contact-details'>
+
+{/* left side */}
 
       <div className='left-side'>
         <h2 className='left-side-heading'>Get in Touch</h2>
         <p className='left-side-text'>Feel free to contact us via email, phone, or visit our office. We look forward to hearing from you!</p>
         <ul className='contact-list'>
-          <li>Email: <a href="mailto:" className='contact-link'>joellembithi@gmail.com </a>
-          <span className='contact-info'>Typically respond within 24 hours</span> </li>
-          <li>Phone: <a href="tel:" className='contact-link'>0743861565 </a>
-          <span className='contact-info'>Mon-Fri, 9am-5pm EST</span></li>
+          <li className='list'><span className='list-title'>Email:</span> <a href="mailto:" className='contact-link'> <br />joellembithi@gmail.com </a>
+          <span className='contact-info'><br />Typically respond within 24 hours</span> </li>
+          <li className='list'><span className='list-title'>Phone:</span> <a href="tel:" className='contact-link'><br />0743861565 </a>
+          <span className='contact-info'><br />Mon-Fri, 9am-5pm EST</span></li>
 
 
-          <li>Office Address: <a href="https://www.google.com/maps" target="_blank" rel="noopener noreferrer" className='contact-link'>View on Map</a>
-          <span>123 Commerce Street<br />
+          <li className='list'><span className='list-title'>Office Address:</span> <a href="https://www.google.com/maps" target="_blank" rel="noopener noreferrer" className='contact-link'><br />View on Map</a>
+          <span><br />123 Commerce Street<br />
 Nairobi, KE 10001</span>
 </li>   
         </ul>
@@ -48,6 +50,7 @@ Nairobi, KE 10001</span>
   <button className="send-button">Send Message</button>
 </div>
 
+      </div>
     </div>
   )
 }
