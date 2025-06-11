3080aaee4c304257035a6d7eddfcba1dcd9a9753
paul <ampaulkaranja@gmail.com>
2025-06-11 22:08:25 +0300

    slightly modified the contact page header

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -5,7 +5,8 @@ const Contacts = () => {
     <div className='container'>
       <div className='contact'>
         <h1 className='heading'>Contact Us</h1>
-      <p className='heading-text'>Have questions or need assistance? We're here to help! <br /> Reach out to our team through any of the channels below.</p>
+      <p className='heading-text'><span>Have questions or need assistance?</span> We're here to help! <br /> </p>
+      <p className='heading-text2'>Reach out to our team through any of the channels below.</p>
       </div>
 
       <div className='contact-details'>
@@ -16,7 +17,7 @@ const Contacts = () => {
         <h2 className='left-side-heading'>Get in Touch</h2>
         <p className='left-side-text'>Feel free to contact us via email, phone, or visit our office. We look forward to hearing from you!</p>
         <ul className='contact-list'>
-          <li className='list'><span className='list-title'>Email:</span> <a href="mailto:" className='contact-link'> <br />joellembithi@gmail.com </a>
+          <li className='list'><span className='list-title'>Email:</span> <a href="mailto:" className='contact-link'> <br />quickcartteam@gmail.com </a>
           <span className='contact-info'><br />Typically respond within 24 hours</span> </li>
           <li className='list'><span className='list-title'>Phone:</span> <a href="tel:" className='contact-link'><br />0743861565 </a>
           <span className='contact-info'><br />Mon-Fri, 9am-5pm EST</span></li>
